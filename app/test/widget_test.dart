225f3cfdb348fe8915d71e998d879b32375c715e
Larry E. Alexander <Larry.alexander@acnc-md.email>
2025-09-26 18:32:08 -0400

    Fix test import to use correct package name

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app/main.dart';
+import 'package:ts4_data_manager/main.dart';
 
 void main() {
   testWidgets('App builds smoke test', (WidgetTester tester) async {
